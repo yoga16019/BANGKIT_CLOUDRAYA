a6b074c123fad8df17cd63d22615a80c92651ed4
unknown <ainurrizkyooo@gmail.com>
2023-06-09 02:31:09 +0800

    update app

--- MD/CloudRaya/app/src/main/java/com/example/cloudraya/Model/UserRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.example.cloudraya.Model
-
-data class UserRequest(
-    var app_key: String? = null,
-    var secret_key: String? = null
-)
--- MD/CloudRaya/app/src/main/java/com/example/cloudraya/Util/NetworkResult.kt
+++
@@ -1,7 +0,0 @@
-package com.example.cloudraya.Util
-
-sealed class NetworkResult{
-    data class Success<out T>(val data: T): NetworkResult()
-    data class Error(val exception: Throwable): NetworkResult()
-    data class Loading(val isLoading: Boolean): NetworkResult()
-}
